c5a280064035fb7cc544fc2b0a044a2dacb49008
stevebrush <steve.brush@blackbaud.com>
2016-08-11 15:18:50 -0400

    Initial commit

---
+++ app/hero.service.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class HeroService {
+  getHeroes() {
+    return Promise.resolve(HEROES);
+  }
+  getHero(id: number) {
+    return this.getHeroes().then(heroes =>
+        heroes.find(hero => hero.id === id)
+    );
+  }
+}
